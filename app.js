0909db92b6c7cc284f32f6b2c35a761ce0cdd689
sakaspar <travis@travis-ci.org>
2021-05-10 14:20:03 +0100

    yes

---
+++ app.js
@@ -0,0 +1,50 @@
+let MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+let express = require('express');
+let app = express();
+app.listen(3000, () => console.log('Server running on port 3000!'))
+
+/*
+app.get('/:name', (req, res) => {
+    res.send('Your name is ' + req.params.name + '\n');
+});*/
+
+/*app.use(express.json());
+app.post('/', (req, res) => {
+    res.json(req.body);
+});*/
+
+
+
+app.get('/:name', (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("users");
+        dbo.collection("customers").findOne({
+            name: req.params.name
+        }, 
+        function(err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
+});
+
+
+
+app.post('/', (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("users");
+        dbo.collection("customers").insertOne({
+            name: req.body.name,
+            age: req.body.age
+        }, 
+        function(err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
+});

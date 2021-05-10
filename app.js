581fc36711faebabcba5a5f6af59675186a1556e
sakaspar <travis@travis-ci.org>
2021-05-10 16:04:55 +0100

    Fetsh update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var url = "mongodb://localhost:27017/";
 let express = require('express');
 let app = express();
 app.listen(3000, () => console.log('Server running on port 3000!'))
+var found=false;
 
 /*
 app.get('/:name', (req, res) => {
@@ -16,6 +17,8 @@ app.post('/', (req, res) => {
 
 
 
+
+
 app.get('/:name', (req, res) => {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
@@ -26,25 +29,43 @@ app.get('/:name', (req, res) => {
         function(err, result) {
             if (err) throw err;
             res.json(result);
-            db.close();
+            var found=True;
+            /*Update*/
+            var myquery = { age: "55" };
+            var newvalues = { $set: {name: "Mickey", age: "Canyon 23" } };
+            dbo.collection("hamza").updateOne(myquery, newvalues, function(err, db) {
+              if (err) throw err;
+              console.log("1 document updated");
+              db.close();
+            });
         });
     });
 });
 
 
-
-app.post('/', (req, res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("hamza");
-        dbo.collection("hamza").insertOne({
-            name: req.body.name,
-            age: req.body.age
-        }, 
-        function(err, result) {
+if (found==false) {
+    app.post('/', (req, res) => {
+        MongoClient.connect(url, function(err, db) {
             if (err) throw err;
-            res.json(result);
-            db.close();
+            var dbo = db.db("hamza");
+            dbo.collection("hamza").insertOne({
+                name: req.body.name,
+                age: req.body.age
+            }, 
+            function(err, result) {
+                if (err) throw err;
+                res.json(result);
+                db.close();
+   
+            });
         });
     });
-});
+    
+  }
+
+  
+    
+
+
+
+  
